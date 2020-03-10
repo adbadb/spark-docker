2d8d04e0583c8b4a6b0cb95ba4bdb5f4e44711d3
Alexander Breyman <a@breyman.ru>
2020-03-10 11:43:12 +0300

    Remove scala and sbt

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
-docker build -t spark-base:2.4.5 ./docker/base
-docker build -t spark-master:2.4.5 ./docker/master
-docker build -t spark-worker:2.4.5 ./docker/worker
-docker build -t spark-edge:2.4.5 ./docker/edge
+docker build -t adbadb/spark-base:2.4.5 ./docker/base
+docker build -t adbadb/spark-master:2.4.5 ./docker/master
+docker build -t adbadb/spark-worker:2.4.5 ./docker/worker
+docker build -t adbadb/spark-edge:2.4.5 ./docker/edge
+docker push adbadb/spark-base:2.4.5
+docker push adbadb/spark-worker:2.4.5
+docker push adbadb/spark-edge:2.4.5
