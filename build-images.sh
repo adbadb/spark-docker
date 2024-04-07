25a0f3c3e5b520cbdf090a41e7f4c8384d7a2d79
Alexander Breyman <adbadb@gmail.com>
2024-04-08 01:30:35 +0300

    move to Spark 3.5.1

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+TAG=3.5.1
 set -e
-docker build -t adbadb/spark-base:2.4.5 ./docker/base
-docker build -t adbadb/spark-master:2.4.5 ./docker/master
-docker build -t adbadb/spark-worker:2.4.5 ./docker/worker
-docker build -t adbadb/spark-edge:2.4.5 ./docker/edge
-docker push adbadb/spark-base:2.4.5
-docker push adbadb/spark-worker:2.4.5
-docker push adbadb/spark-edge:2.4.5
+docker build -t adbadb/spark-base:$TAG ./docker/base
+docker build -t adbadb/spark-master:$TAG ./docker/master
+docker build -t adbadb/spark-worker:$TAG ./docker/worker
+docker build -t adbadb/spark-edge:$TAG ./docker/edge
+docker push adbadb/spark-base:$TAG
+docker push adbadb/spark-worker:$TAG
+docker push adbadb/spark-edge:$TAG
